6b5450d6cd8ccece6d11f1fd95b64c336c15dbbd
Omar Gastelum <ogastelum@collabtime.co>
2023-11-30 16:46:59 -0800

    Test deleting images

--- controllers/upload.controller.ts
+++ controllers/upload.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 export const upload = (req: Request, res: Response) => {
     console.log("UPLOAD", req.file);
     try {
-        res.send({filename: req.file?.filename});
+        res.send({filename: req.file?.filename})
     } catch (error) {
         res.status(400).send({success: false})
     }
