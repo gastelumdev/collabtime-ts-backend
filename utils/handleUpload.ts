23ba520cd9c048bcb86d433c67c27634af0b5a93
Omar Gastelum <ogastelum@collabtime.co>
2023-12-01 09:42:46 -0800

    Add environtment vars

--- utils/handleUpload.ts
+++ utils/handleUpload.ts
@@ -1,9 +1,12 @@
 import {v2 as cloudinary} from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 cloudinary.config({ 
-    cloud_name: 'dcwwotk0l', 
-    api_key: '656962683321671', 
-    api_secret: 'ws10jQ3LAshZgVL0apjwcKNHRjY' 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_API_KEY, 
+    api_secret: process.env.CLOUD_API_SECRET,
   });
 
 async function handleUpload(file: any) {
