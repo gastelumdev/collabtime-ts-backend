d6c3245a257ad69fba74ede61f77fe92670e5363
Omar Gastelum <ogastelum@collabtime.co>
2024-04-30 12:44:35 -0700

    Fix time comparison

--- index.ts
+++ index.ts
@@ -196,10 +196,10 @@ const deleteOldNotifications = async () => {
 
 // scheduledReminders();
 
-cron.schedule("0 * * * * *", () => {
-  console.log("Another minute by")
-  scheduledReminders();
-})
+// cron.schedule("0 * * * * *", () => {
+//   console.log("Another minute by")
+//   scheduledReminders();
+// })
 
 if (process.env.APP_ENVIRONMENT === "production") {
 
@@ -211,10 +211,10 @@ if (process.env.APP_ENVIRONMENT === "production") {
     setReminders()
   })
 
-  // cron.schedule("0 * * * * *", () => {
-  //   console.log("Another minute by")
-  //   scheduledReminders();
-  // })
+  cron.schedule("0 * * * * *", () => {
+    console.log("Another minute by")
+    scheduledReminders();
+  })
 
   cron.schedule("0 0,15,30,45 * * * *", () => {
     setCrititcalReminders()
