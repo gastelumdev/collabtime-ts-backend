8c1cf9020a4676ceaf325910e77321404d7621a1
Omar Gastelum <ogastelum@collabtime.co>
2024-06-02 17:16:51 -0700

    Refactor updateWorkspace function

--- models/workspace.model.ts
+++ models/workspace.model.ts
@@ -38,3 +38,21 @@ export const getWorkspaceById = async (id: string) => {
     const workspace = await Workspace.findById(id);
     return workspace;
 }
+
+/**
+ * Updates a workspace document in the database by its ID.
+ *
+ * This asynchronous function searches the `Workspace` collection for a document with the specified ID
+ * and updates it with the provided new workspace data. It returns the updated workspace document.
+ *
+ * @param {string} id - The unique identifier of the workspace to update.
+ * @param {IWorkspace} newWorkspace - An object containing the new data for the workspace.
+ * @returns {Promise<Object|null>} - A promise that resolves to the updated workspace document
+ *                                   if the update is successful, or null if no workspace is found with the given ID.
+ */
+export const getWorkspaceByIdAndUpdate = async (id: string, newWorkspace: IWorkspace) => {
+    console.log({ newWorkspace, id })
+    const workspace = await Workspace.findByIdAndUpdate(id, newWorkspace, { new: true });
+    console.log({ workspace })
+    return workspace;
+}
